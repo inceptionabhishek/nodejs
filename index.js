b5d473eeba9c2da267624ac1573d06848ec43714
Abhishek kumar <abhishek1122338@gmail.com>
2022-04-26 17:59:57 +0530

    Created A Nodejs Api #connectedroutes

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+const app = express();
+
+require("dotenv").config({ path: ".env" });
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+
+app.listen(PORT, () => {
+  console.log("Server is running on port :  " + PORT);
+});
